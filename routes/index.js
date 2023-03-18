637307855d82bdc6ec4912870a567a6a78b604a0
Aps2612 <apsingh1722@gmail.com>
2023-03-18 13:03:59 +0530

    nothing

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,23 @@
-const express = require('express');
-const router = express.Router();
+
 //import express and router variable of that express
 //router is like app that lives inside main application
 //now we will create routes
 
-router.get('/',(req,res)=>{
-    res.render('index');
-});
+const express = require('express')
+const router = express.Router()
+const Book = require('../models/book')
+
+router.get('/', async (req, res) => {
+  let books
+  try {
+    books = await Book.find().sort({ createdAt: 'desc' }).limit(10).exec()
+  } catch {
+    books = []
+  }
+  res.render('index', { books: books })
+})
+
+module.exports = router
 //res.send ki jagah res.render kaer diye ek file ko
 //render aise hoga ===>(views folder -> layouts folder -> layout file(when finds body)->index.js->remaining layout)
 
